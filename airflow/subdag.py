f452bc39bebdb2691bd35ab8af8b5dbba040661a
Sarah Eaglesfield <speedbunny@gmail.com>
2023-01-30 12:25:08 +0000

    Create subdag.py

---
+++ airflow/subdag.py
@@ -0,0 +1,39 @@
+#From official Airflow repo
+
+"""Helper function to generate a DAG and operators given some arguments."""
+from __future__ import annotations
+
+# [START subdag]
+import pendulum
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+
+def subdag(parent_dag_name, child_dag_name, args) -> DAG:
+    """
+    Generate a DAG to be used as a subdag.
+    :param str parent_dag_name: Id of the parent DAG
+    :param str child_dag_name: Id of the child DAG
+    :param dict args: Default arguments to provide to the subdag
+    :return: DAG to use as a subdag
+    """
+    dag_subdag = DAG(
+        dag_id=f"{parent_dag_name}.{child_dag_name}",
+        default_args=args,
+        start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+        catchup=False,
+        schedule="@daily",
+    )
+
+    for i in range(5):
+        EmptyOperator(
+            task_id=f"{child_dag_name}-task-{i + 1}",
+            default_args=args,
+            dag=dag_subdag,
+        )
+
+    return dag_subdag
+
+
+# [END subdag]
